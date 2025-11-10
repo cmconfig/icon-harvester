752fa720186da45eb465608eab32f0f5dabda3cf
cmconfig <jamiegelkiehgfh4@outlook.com>
2025-11-10 22:56:28 +0100

    Delete server directory

--- server/views/layouts/theme.layout.php
+++
@@ -1,7 +0,0 @@
-<?php
-Layout::extend('layouts/master');
-Layout::input($title, 'string');
-Layout::input($body, 'Block');
-
-$navigation = Part::block('parts/navigation');
-?>
--- server/views/parts/navigation.part.php
+++
@@ -1,6 +0,0 @@
-<ul>
-	<li><?php echo Html::anchor(Url::action('IconHarvesterHomeController::index'), 'Home') ?></li>
-	<!-- 
-	Add your application's navigation links here.
-	-->
-</ul>
